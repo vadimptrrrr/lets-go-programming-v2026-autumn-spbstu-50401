8c1d950de45731c26b035a75c47e13f136a9bc23
Vadim Petrov <vadikdydy@maol.ru>
2026-09-13 11:39:58 +0300

    [TASK-1] Work done

--- vadim.petrov/task-1/cmd/service/main.go
+++ vadim.petrov/task-1/cmd/service/main.go
@@ -1,3 +1,50 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+)
+
+func main() {
+	var firstNum, secondNum int
+	var operation string
+
+	_, err := fmt.Scan(&firstNum)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+
+	_, err = fmt.Scan(&secondNum)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+
+	_, err = fmt.Scan(&operation)
+	if err != nil {
+		fmt.Println("Invalid operation")
+		return
+	}
+
+	switch operation {
+	case "+":
+		fmt.Println(firstNum + secondNum)
+		return
+	case "-":
+		fmt.Println(firstNum - secondNum)
+		return
+	case "*":
+		fmt.Println(firstNum * secondNum)
+		return
+	case "/":
+		if secondNum == 0 {
+			fmt.Println("Division by zero")
+			return
+		}
+		fmt.Println(firstNum / secondNum)
+		return
+	default:
+		fmt.Println("Invalid operation")
+		return
+	}
+}
